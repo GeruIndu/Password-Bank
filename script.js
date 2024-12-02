591538115c2b8be4da7820555c1f770ec952e628
GeruIndu <indrajitmondal284@gmail.com>
2024-12-02 23:54:13 +0530

    add functionality of empty and duplicate key

--- script.js
+++ script.js
@@ -8,38 +8,55 @@ function addCard(websiteName, userName, password) {
     div.innerHTML = `<div class="website">${websiteName}</div>
                      <div class="user">${userName}</div>
                      <div class="pass">${password}</div>
-                     <button class="del" data-key="${websiteName}">Delete</button>`; 
+                     <button class="del" data-key="${websiteName}">Delete</button>`;
     container.append(div);
 }
 
 // Function to delete an item from local storage and update the UI
 function deleteItem(key) {
-    localStorage.removeItem(key); 
+    localStorage.removeItem(key);
     location.reload();
 }
 
+function checkPresent(key) {
+    for (let i = 0; i < localStorage.length; i++) {
+        let id = localStorage.key(i);
+        if (id == key)
+            return true;
+    }
+    return false;
+}
+
 // Form submission to save data in local storage
-document.querySelector(".form").addEventListener('submit', (e) => {
+let form = document.querySelector(".form");
+form.addEventListener('submit', (e) => {
     let web_name = document.querySelector('.web-name');
+    console.log(web_name.value == null);
     let user_name = document.querySelector('.user-name');
     let password = document.querySelector('.password');
+    let error = document.querySelector(".Error");
+    
     e.preventDefault();
-
     let data = {
         user: user_name.value.trim(),
         pass: password.value.trim()
     };
 
-    localStorage.setItem(`${web_name.value.trim()}`, JSON.stringify(data));
-    location.reload();
+    if (checkPresent(web_name.value))
+        error.style.display = "block";
+    else {
+        localStorage.setItem(`${web_name.value.trim()}`, JSON.stringify(data))
+        location.reload();
+    }
+
 });
 
 // Event Delegation for Delete Buttons
 container.addEventListener('click', (e) => {
     console.log(e.target.classList);
     if (e.target.classList.contains('del')) {
-        const key = e.target.getAttribute('data-key'); 
-        deleteItem(key); 
+        const key = e.target.getAttribute('data-key');
+        deleteItem(key);
     }
 });
 
