31852432b9060b924aab3bbe4e5a2c3878f8cc1d
GeruIndu <indrajitmondal284@gmail.com>
2024-12-04 22:32:47 +0530

    Add edit Save Button

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ function addCard(websiteName, userName, password) {
     let div = document.createElement('div');
     div.setAttribute('class', 'card');
 
-    div.innerHTML = `<div class="website">${websiteName}</div>
-                     <div class="user">${userName}</div>
-                     <div class="pass">${password}</div>
-                     <button class="del" data-key="${websiteName}">Delete</button>`;
+    div.innerHTML = `   <div class="website">${websiteName}</div>
+                        <div class="userData">
+                            <div class="user">${userName}</div>
+                            <input type="text" value="${userName}" class="input edit-user">
+                        </div>
+                        <div class="password">
+                            <div class="pass">${password}</div>
+                            <input type="text" value="${password}" class="input edit-pass">
+                        </div>
+                        <div class="btns">
+                            <button class="del" data-key="${websiteName}">Delete</button>
+                            <button class="edit" data-key="${websiteName}">Edit</button>
+                            <button class="edit-save" data-key="${websiteName}">Save</button>
+                        </div>`;
     container.append(div);
 }
 
@@ -31,12 +41,12 @@ function checkPresent(key) {
 let form = document.querySelector(".form");
 form.addEventListener('submit', (e) => {
     let web_name = document.querySelector('.web-name');
-    console.log(web_name.value == null);
     let user_name = document.querySelector('.user-name');
     let password = document.querySelector('.password');
     let error = document.querySelector(".Error");
-    
+
     e.preventDefault();
+
     let data = {
         user: user_name.value.trim(),
         pass: password.value.trim()
@@ -53,10 +63,40 @@ form.addEventListener('submit', (e) => {
 
 // Event Delegation for Delete Buttons
 container.addEventListener('click', (e) => {
-    console.log(e.target.classList);
+    // console.log(e.target.classList);
     if (e.target.classList.contains('del')) {
         const key = e.target.getAttribute('data-key');
         deleteItem(key);
+    } 
+
+    // when click on edit button
+    if(e.target.classList.contains('edit')) {
+        const key = e.target.getAttribute('data-key');
+        const card = e.target.closest('.card');
+
+        card.querySelector('.user').style.display = 'none';
+        card.querySelector('.pass').style.display = 'none';
+        card.querySelector(".edit").style.display = 'none';
+        card.querySelector('.edit-user').style.display = 'inline-block';
+        card.querySelector('.edit-pass').style.display = 'inline-block';
+        card.querySelector(".edit-save").style.display = 'inline-block';
+    }
+
+    // now save the edit
+    if(e.target.classList.contains('edit-save'))
+    {
+        let key = e.target.getAttribute('data-key');;
+        const card = e.target.closest('.card');
+        let update_user = card.querySelector('.edit-user').value.trim();
+        let update_pass = card.querySelector('.edit-pass').value.trim();
+
+        let data = {
+            user: update_user,
+            pass: update_pass
+        };
+
+        localStorage.setItem(key, JSON.stringify(data));
+        location.reload();
     }
 });
 
